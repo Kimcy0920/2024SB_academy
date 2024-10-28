551d164791f3c63ac97533b1239e676970534707
Kimcy0920 <qkqhzmfjddl@naver.com>
2024-10-28 19:40:14 +0900

    sb1028

--- sb1024/src/main/java/edu/du/sb1024/controller/MemberController.java
+++ sb1024/src/main/java/edu/du/sb1024/controller/MemberController.java
@@ -6,6 +6,7 @@ import edu.du.sb1024.entity.RegisterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,12 +46,19 @@ public class MemberController {
         return "redirect:/register/step1";
     }
 
-    @PostMapping("/register/step3")
-    public String handleStep3(RegisterRequest regReq) {
+    @PostMapping("/register/step3") // RegisterRequest 커맨드 객체
+    public String handleStep3(RegisterRequest regReq, BindingResult errors) { // (regReq, errors)프레임워크가 알아서 객체 생성해줌
+        // BindingResult errors = Errors errors
+        // RegisterRequestValidator -> @Override public void validate(Object target(1), Errors errors(2)) {
+        new RegisterRequestValidator().validate(regReq, errors);
+        if (errors.hasErrors())
+            return "register/step2"; // 에러 발생시 step2로 리턴
         try {
             memberRegisterService.regist(regReq);
             return "register/step3";
         } catch (DuplicateMemberException ex) {
+//			errors.rejectValue("email", "duplicate"); // label.properties -> duplicate.email=중복된 이메일입니다.
+            errors.reject("notMatchingPassword"); // p.335
             return "register/step2";
         }
     }
